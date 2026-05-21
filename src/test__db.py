6fcf4d092905255eabb259ab33482f840a124567
shreyjeetdas <iamshreyjeet@gmail.com>
2026-05-21 16:16:23 +0530

    Integrated PostgreSQL with Python

--- src/test__db.py
+++ src/test__db.py
@@ -1,13 +1,34 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="finance_db",
-    user="postgres",
-    password="Papun@23",
-    port="5432"
-)
+conn = None
+cur = None
 
-print("Connected successfully!")
+try:
+    conn = psycopg2.connect(
+        host="localhost",
+        database="finance_db",
+        user="postgres",
+        password="Papun@23",
+        port="5432"
+    )
+    print("Connected successfully!")
 
-conn.close()
+    cur = conn.cursor()
+    cur.execute("""
+    INSERT INTO transactions (date, amount, category, description)
+    VALUES (%s, %s, %s, %s)
+""", ("2026-08-20", 500, "Expense", "Food"))
+    conn.commit()
+
+    print("Transaction inserted!")
+
+except Exception as e:
+    print(e)
+
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
+
+
